77b092c1318aa96047ebf4442281e339a1e7d5c7
Sumeet <sumeetp987@gmail.com>
2024-03-21 10:59:35 +0530

    practice done

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const port = 3005;
 app.use(express.static("public"));
 
 
-const jokeUrl = "https://v2.jokeapi.dev/joke/Any?format=txt"
+const jokeUrl = "https://v2.jokeapi.dev/joke/Any?format=txt";
 
 app.get("/", async (req, res) => {
     try{
-        let result = await axios.get(jokeUrl)
-        let joke = result.data
-        console.log(result.data)
-        res.render("index.ejs", {joke : joke})
+        let result = await axios.get(jokeUrl);
+        let joke = result.data;
+        console.log(result.data);
+        res.render("index.ejs", {joke : joke});
     }catch(error){
-        console.error("Error:", error.message)
-        res.status(500).send("An error occured while fetching the joke.")
+        console.error("Error:", error.message);
+        res.status(500).send("An error occured while fetching the joke.");
     }
 });
 
